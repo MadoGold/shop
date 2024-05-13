1f336073913e905f3ea7d7434e6af1b609ed5fff
MadoGold <82536105+MadoGold@users.noreply.github.com>
2024-05-13 05:28:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,21 +26,20 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <Script type="text/javascript" >
+        <Layout>{children}</Layout>
+        <Script id='metrika-counter' strategy='afterInteractive'>
           {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(97261094, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-          });`}
+            ym(97261094, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });`}
         </Script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/97261094" style={{ position: "absolute", left: '-9999px' }} alt="" /></div></noscript>
-        <Layout>{children}</Layout>
       </body>
     </html>
   )
